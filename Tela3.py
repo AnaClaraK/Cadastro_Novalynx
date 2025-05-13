b4166cace6ec74d76e3c6e50ac3f32638bbc3eac
Ana Clara Cação Da Cruz <Ana Clara Cao Da Cruz>
2025-05-13 11:14:00 -0300

    No caminho

--- Tela3.py
+++ Tela3.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+#NÃOFUNCIONA
 #criar função para add os itens na lista
 
 lista = []
--- TelaPrincipal.py
+++ TelaPrincipal.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import subprocess
-
+#FUNCIONA
 janela = tk.Tk()
 janela.title("Pagina Principal")
 janela.geometry("200x100")
